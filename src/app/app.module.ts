5d14d0740122a9e864c8dfd8e2f185095c7c40f1
ashutosh-k <ashutoshs@dewsolutions.in>
2021-05-08 14:08:05 +0530

    refresh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BookListComponent } from './book-list/book-list/book-list.component';
-import { BookCollectionComponent } from './book-collection/book-collection/book-collection.component';
 import { StoreModule } from '@ngrx/store';
-import { booksReducer } from './state/books.reducer';
-import { collectionReducer } from './state/collection.reducer';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BookListComponent,
-    BookCollectionComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    StoreModule.forRoot({ books: booksReducer, collection: collectionReducer })
+    StoreModule.forRoot({ })
   ],
   providers: [],
   bootstrap: [AppComponent]
